9477ae4aba34fff4f61e3bc17bceb189a0bfd619
sma.student <sma.student@interia.pl>
2018-09-26 10:26:26 +0200

    1

--- functions.py
+++ functions.py
@@ -4,13 +4,13 @@ import time
 def sendAndReceive(serialObject, message):
     serialObject.write(bytearray(message, 'utf-8'))
     time.sleep(0.2)
-    ans = serialObject.read(8)
+    ans = serialObject.readline()
     return ans
 
 def sendAndReadFirst(serialObject):
     serialObject.write(b'SRQ?')
     time.sleep(0.2)
-    ans = serialObject.read(8)
+    ans = serialObject.readline()
     return ans
 
 
