8d98df2fe04adb7e5d96144ec47da73a157c5fc2
sma.student <sma.student@interia.pl>
2018-10-04 14:17:26 +0200

    replace some code

--- testing.py
+++ testing.py
@@ -7,13 +7,14 @@ from functions import *
 chamber1 = Chamber()
 
 for i in range(10):
-    # time.sleep(1)
-    # chamber1.showTemp()
-    # print('')
-    # #chamber1.humiData()
-    # print('')
-    # print('')
+    time.sleep(1)
     chamber1.update()
-    # chamber1.tempData()
-    # print("")
+    chamber1.showTemp()
+    #print('')
+    #chamber1.humiData()
+    # print('')
+    # print('')
+
+    chamber1.tempData()
+    print("")
     # print("")
