3d5ef83220c3eb52cf1ec0940705bb5152f056af
sma.student <sma.student@interia.pl>
2018-10-04 12:46:18 +0200

    1

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import time
 import serial
 import datetime
 
+
 def sendAndReceive(serialObject, message):
     serialObject.write(bytearray(message, 'utf-8'))
     time.sleep(0.2)
@@ -9,15 +10,59 @@ def sendAndReceive(serialObject, message):
     ans = ans.decode("utf-8")
     return ans
 
+
 def saveTableToFile(obj, table):
-        for i in table:
-            obj.write(str(i))
-            obj.write('\n')
-        obj.close()
+    for i in table:
+        obj.write(str(i))
+        obj.write('\n')
+    obj.close()
 
-def changeAnsToTable(ans):
-    return 'aa'
 
+def retFloatFromString(givStr):
+    retFloat = 0.0
+    newFirstStr = str()
+    newLastStr = str()
+    wasThereComma = False
+
+    for c in givStr:
+        if c != '.' and not wasThereComma:
+            newFirstStr = newFirstStr + c
+        elif c != '.' and wasThereComma:
+            newLastStr = newLastStr + c
+        elif c == '.':
+            wasThereComma = True
+    retFloat = float(int(newFirstStr)) + (float(int(newLastStr)) / pow(10, (len(newLastStr))))
+    return retFloat
+
+
+def changeAnsForTable(ans):
+    PV = 0.0
+    SP = 0.0
+    low = 0.0
+    max = 0.0
+    tempStr = str()
+    wasThereComma = False
+    iteration = 0
+
+    for c in ans:
+        if c != ',':
+            tempStr = tempStr + c
+        elif c == ',':
+            if iteration == 0:
+                PV = retFloatFromString(tempStr)
+                tempStr = str()
+            elif iteration == 1:
+                SP = retFloatFromString(tempStr)
+                tempStr = str()
+            elif iteration == 2:
+                max = retFloatFromString(tempStr)
+                tempStr = str()
+            elif iteration == 3:
+                low = retFloatFromString(tempStr)
+                tempStr = str()
+            iteration = iteration + 1
+
+    return PV, SP, max, low
 
 ############### OLD ONE ##################
 #
--- testing.py
+++ testing.py
@@ -1,14 +1,27 @@
 import numpy as np
 import time
-# import MySQLdb
+ #import MySQLdb
 from chamberClass import Chamber
+from functions import *
 
 chamber1 = Chamber()
-arrayNp = np.array([0,0,0,0])
+arrayNp = np.array(chamber1.showTemp())
 
 for i in range(10):
     time.sleep(1)
-    np.append(arrayNp, chamber1.showTemp())
+    print(chamber1.showTemp())
+    print(changeAnsForTable())
 
 for i in arrayNp:
     print(i)
+
+
+
+
+
+
+
+
+
+
+
